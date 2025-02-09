31231f4e057dea6cf96e146f8b5489fb4bc2730e
Aaron Siddiky <156956166+AaronSiddiky@users.noreply.github.com>
2025-02-08 19:06:48 -0500

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,28 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export default clerkMiddleware({
-  publicRoutes: ["/", "/books", "/pricing"],
-  debug: true // Add this to see more detailed errors
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: [
+    "/",
+    "/books",
+    "/pricing",
+    "/api/.*",
+    "/about",
+    "/_next/static/.*",
+  ],
+  ignoredRoutes: [
+    "/api/clerk",
+    "/_next/image",
+    "/favicon.ico",
+  ],
 });
 
 export const config = {
   // Protects all routes, including api/trpc
   // See https://clerk.com/docs/references/nextjs/auth-middleware
   // for more information about configuring your middleware
-  matcher: [
-    "/((?!.*\\..*|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-    "/((?!_next/static|_next/image|favicon.ico).*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 }; 
