6966842f61186e94d50840d9838ff5bf91117ece
LeonardHolter <leonard.holter@columbia.edu>
2025-02-09 19:44:23 -0500

    test

--- middleware.ts
+++ middleware.ts
@@ -5,18 +5,22 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/books",
-    "/pricing",
-    "/api/create-checkout-session",
-    "/books/6",
-  ],
+  publicRoutes: ["/(.*)", "/api(.*)", "/trpc(.*)"],
 });
 
 export const config = {
   // Protects all routes, including api/trpc
   // See https://clerk.com/docs/references/nextjs/auth-middleware
   // for more information about configuring your middleware
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public (public files)
+     */
+    "/((?!.+\\.[\\w]+$|_next|favicon\\.ico|public).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
