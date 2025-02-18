2a351996d48cce076c03b1be2f10506484decfdf
LeonardHolter <leonard.holter@columbia.edu>
2025-02-17 21:25:03 -0500

    purge

--- components/Header.tsx
+++
@@ -1,9 +0,0 @@
-export default function Header() {
-  return (
-    <div className="fixed top-4 right-4 z-10">
-      <button className="bg-neutral-800 hover:bg-neutral-700 text-white px-4 py-2 rounded-full">
-        L
-      </button>
-    </div>
-  );
-}
